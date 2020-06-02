a5c15321dcbafdf88bce001e65c6e7d9ce542e90
cskiwi <glenn.latomme@gmail.com>
2020-06-02 14:45:50 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatInputModule } from '@angular/material/input';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { SearchBarComponent } from './search/components/search-bar/search-bar.component';
-import { SearchResultsComponent } from './search/pages/search-results/search-results.component';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatInputModule } from "@angular/material/input";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SearchBarComponent } from "./search/components/search-bar/search-bar.component";
+import { SearchResultsComponent } from "./search/pages/search-results/search-results.component";
 
+const baseModules = [BrowserModule, BrowserAnimationsModule, HttpClientModule];
 const material = [MatToolbarModule, MatInputModule, MatCheckboxModule];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, material, HttpClientModule],
+  imports: [baseModules, FormsModule, material],
   declarations: [AppComponent, SearchBarComponent, SearchResultsComponent],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
